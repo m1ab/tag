6ba4353bedf800574a85236391184dc526345f8a
Крюков Михаил <m.kryukov@protonservice.ru>
2016-03-03 13:05:36 +0300

    add bs tables and table tags

---
+++ tag-bs/src/main/java/ru/lumo/html/bs/tag/BsTable.java
@@ -0,0 +1,37 @@
+package ru.lumo.html.bs.tag;
+
+import ru.lumo.html.tag.Tbody;
+import ru.lumo.html.tag.Thead;
+
+/**
+ * Created by misha on 03.03.16.
+ */
+public class BsTable extends BsTag {
+
+    private Thead thead;
+    private Tbody tbody;
+
+    public enum Type {
+        TABLE("table"),
+        TABLE_STRIPPED("table table-striped"),
+        TABLE_BORDERED("table table-bordered"),
+        TABLE_HOVER("table table-hover"),
+        TABLE_CONDENCED("table table-condensed");
+        private final String name;
+        Type(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return name;
+        }
+    }
+
+    public BsTable(Type type) {
+        super("table", type.getName());
+        thead = new Thead();
+        add(thead);
+        tbody = new Tbody();
+        add(tbody);
+    }
+
+}
---
+++ tag-core/src/main/java/ru/lumo/html/tag/Th.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ru.lumo.html.tag;
+
+/**
+ *
+ * @author misha
+ */
+public class Th extends Tag {
+
+    public Th() {
+        super("th");
+        add("");
+    }
+}

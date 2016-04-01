7c6d3700608b19f47d73f7a885e9036fec5841a4
misha <misha@lu-mo.ru>
2016-04-02 01:51:47 +0300

    Добавлен тег BsPLead

---
+++ tag-bs/src/main/java/ru/lumo/html/bs/tag/BsPLead.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.lumo.html.bs.tag;
+
+import ru.lumo.html.tag.Tag;
+
+/**
+ *
+ * @author misha
+ */
+public class BsPLead extends BsP {
+
+    public BsPLead() {
+        super("p", "lead");
+    }
+
+    public BsPLead(String s) {
+        this();
+        add(s);
+    }
+
+    public BsPLead(Tag tag) {
+        this();
+        add(tag);
+    }
+}

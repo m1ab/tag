cb050ee50d615549143d2d796160119055ad3953
Mikhail Kryukov <KryukovMV@rbtechnologies.ru>
2017-09-08 13:45:39 +0300

    release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-~/apache-maven-3.3.9/bin/mvn release:clean -Ptotal && \
+~/apache-maven-3.3.9/bin/mvn release:clean && \
 ~/apache-maven-3.3.9/bin/mvn release:prepare -Ptotal && \
 ~/apache-maven-3.3.9/bin/mvn release:perform -Ptotal
